cc0aebcca912cfbeede03dcbdcab9ac8fc1d28c0
Jack Zhu <54791616+JZ-wenxuan@users.noreply.github.com>
2023-03-17 15:46:33 -0500

    Add mediaSession.playbackState

--- src/utils/Player.js
+++ src/utils/Player.js
@@ -483,6 +483,7 @@ export default class {
     if (autoplay && this._currentTrack.name) {
       this._scrobble(this.currentTrack, this._howler?.seek());
     }
+    navigator.mediaSession.playbackState = "none";
     return getTrackDetail(id).then(data => {
       const track = data.songs[0];
       this._currentTrack = track;
@@ -693,6 +694,7 @@ export default class {
     if (trackID === undefined) {
       this._howler?.stop();
       this._setPlaying(false);
+      navigator.mediaSession.playbackState = "none";
       return false;
     }
     this.current = index;
@@ -771,6 +773,7 @@ export default class {
     this._howler?.once('fade', () => {
       this._howler?.pause();
       this._setPlaying(false);
+      navigator.mediaSession.playbackState = 'paused';
       setTitle(null);
       this._pauseDiscordPresence(this._currentTrack);
     });
@@ -784,6 +787,7 @@ export default class {
       this._howler?.fade(0, this.volume, PLAY_PAUSE_FADE_DURATION);
 
       this._setPlaying(true);
+      navigator.mediaSession.playbackState = "playing";
       if (this._currentTrack.name) {
         setTitle(this._currentTrack);
       }
